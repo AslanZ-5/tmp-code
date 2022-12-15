8eba59e3885809d386862282f69f8bb1d9247048
мансур зурабов <asl.zurabov@gmail.com>
2022-12-15 11:52:04 +0300

    React components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React  from "react";
 import ReactDOM  from "react-dom";
-
-const el = (
-  <div>
-    <h1>My todo List</h1>
-    <input placeholder="search"/>
+const TodoList = () => {
+  return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesom app</li>
-    </ul>
+    <li>Learn React</li>
+    <li>Build Awesom app</li>
+  </ul>
+  )
+}
+const TodoInput = () => {
+  return (
+    <input placeholder="search"/>
+
+  )
+}
+const TodoHeader = () => {
+  return (
+    <h1>My todo List</h1>
+  )
+}
+
+const App = () => {
+  return (
+<div>
+    <TodoHeader/>
+    <TodoInput/>
+   <TodoList/>
   </div>
-);
-ReactDOM.render(el,document.getElementById('root'))
+  )
+};
+
+ReactDOM.render(<App />,document.getElementById('root'))
