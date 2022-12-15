839c28db299d3e2af687fb3303f055603f52dfc1
мансур зурабов <asl.zurabov@gmail.com>
2022-12-15 11:41:07 +0300

    react elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React  from "react";
+import ReactDOM  from "react-dom";
 
+const el = (
+  <div>
+    <h1>My todo List</h1>
+    <input placeholder="search"/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesom app</li>
+    </ul>
+  </div>
+);
+ReactDOM.render(el,document.getElementById('root'))
