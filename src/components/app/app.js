7cc99d845dcee9b54370830acfe29b2553b06430
мансур зурабов <asl.zurabov@gmail.com>
2022-12-18 12:06:17 +0300

    deleting items wisth setState

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,40 @@
-import React  from "react";
+import React, {Component}  from "react";
 import './app.css'
 import TodoHeader from "../todo-header";
 import TodoInput from "../todo-input";
 import TodoList from "../todo-list";
 import ItemStatusFilter from '../item-status-filter';
 
-const App = () => {
-  const todoData = [
-    {label:"Drink some Coffe",id:"1"},
-    {label:"Make Awesome App",id:"2"},
-    {label:"Have a lunch",id:"3"}
-  ]
-  return (
-<div className="todo-app">
-    <TodoHeader toDo={3} done='4'/>
-    <div className="top-panel d-flex">
-    <TodoInput/>
-    <ItemStatusFilter/>
-    </div>
-    
-   <TodoList data={todoData}/>
-  </div>
-  )
+export default class App extends Component{
+  state = {
+    todoData:[
+      {label:"Drink some Coffe",id:"1"},
+      {label:"Make Awesome App",id:"2"},
+      {label:"Have a lunch",id:"3"}
+    ]
+  }
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const inx = todoData.findIndex((el) => el.id === id)
+      const newArr = [
+        ...todoData.slice(0,inx),
+        ...todoData.slice(inx+1)
+      ]
+      return {todoData:newArr}
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app">
+          <TodoHeader toDo={3} done='4'/>
+          <div className="top-panel d-flex">
+          <TodoInput/>
+          <ItemStatusFilter/>
+          </div>
+          
+         <TodoList onDeleted={this.deleteItem} data={this.state.todoData}/>
+        </div>
+        )
+  }
+  
 };
-export default App;
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,10 +1,12 @@
 import React  from "react";
 import TodoListItem from "../todo-list-item";
 import './todo-list.css'
-const TodoList = ({data}) => {
+const TodoList = ({data, onDeleted}) => {
     const todoItems = data.map(item => {
         const {id} = item
-        return <li className="list-group-item" key={id}><TodoListItem {...item} /></li>
+        return <li className="list-group-item" key={id}><TodoListItem 
+         onDeleted={() => onDeleted(id)}
+         {...item} /></li>
     })
     return (
       <ul className="list-group todo-list">
