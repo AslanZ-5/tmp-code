33eea818c79115191c4c48fcfb297d67019c2969
мансур зурабов <asl.zurabov@gmail.com>
2022-12-23 15:27:04 +0300

    d

--- dc-app/src/TemperatureInput.js
+++ dc-app/src/TemperatureInput.js
@@ -8,19 +8,18 @@ export default class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Введите температуру в градусах {scaleNames[scale]}:</legend>
+        <legend>Введите градусы по шкале {scaleNames[scale]}:</legend>
         <input value={temperature} onChange={this.handleChange} />
       </fieldset>
     );
