0828bc12d9fee5c43bce1c218d90754326e3e1e3
мансур зурабов <asl.zurabov@gmail.com>
2022-12-24 10:27:47 +0300

    components header planets persons and items created

--- stars-db/src/components/header/header.js
+++ stars-db/src/components/header/header.js
@@ -1,6 +1,24 @@
 import React from "react";
+import "./header.css";
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className="header d-flex">
+      <h3>
+        <a href="/re">Star DB</a>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <a href="/tre">People</a>
+        </li>
+        <li>
+          <a href="/ds">Planets</a>
+        </li>
+        <li>
+          <a href="/gfd">Starships</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
--- stars-db/src/components/list-item/ListItem.js
+++ stars-db/src/components/list-item/ListItem.js
@@ -1,6 +1,13 @@
 import React from "react";
+import "./ListItem.css";
 const ListItem = () => {
-  return <div>List Item</div>;
+  return (
+    <ul className="item-list list-group">
+      <li className="list-group-item">Luke Skywalker</li>
+      <li className="list-group-item">Darth Vader</li>
+      <li className="list-group-item">R2-D2</li>
+    </ul>
+  );
 };
 
 export default ListItem;
