ded90eb186b196086a3c2e7553738873461a74fa
мансур зурабов <asl.zurabov@gmail.com>
2022-12-24 15:34:49 +0300

    handling errors

---
+++ stars-db/src/components/loading/loading.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./loading.css";
+const Loading = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-787eu0panfc">
+      <div className="ldio-jwbeb70op2">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- stars-db/src/components/random-planet/RandomPlanet.js
+++ stars-db/src/components/random-planet/RandomPlanet.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
 import "./RandomPlanet.css";
 import SwapiService from "../../services/swapi";
+import Loading from "../loading";
+
+const ErrorMessage = () => {
+  return (
+    <div style={{ margin: "40px" }} className="error">
+      <h1>Sorry (:</h1>
+      <h4>Something went wrong</h4>
+    </div>
+  );
+};
+
+const PlanetView = ({ planet }) => {
+  const { name, population, rotationPeriod, diameter, id } = planet;
+  return (
+    <React.Fragment>
+      <img
+        className="planet-image"
+        alt="d"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+      />
+
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
+
 class RandomPlanet extends Component {
   state = {
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null,
+    planet: {},
+    planetLoaging: true,
+    error: false,
   };
   componentDidMount() {
     this.setPlanetState();
@@ -14,39 +55,27 @@ class RandomPlanet extends Component {
   setPlanetState() {
     const id = Math.floor(Math.random() * 19 + 2);
     const req = new SwapiService();
-    req.getPerson(3);
     const planet = req.getPlanet(id);
     // req.getAllPlanets().then((dt) => console.log(dt));
-    planet.then((data) => this.setState(data));
+    planet
+      .then((planet) => {
+        this.setState({ planet });
+        this.setState({ planetLoaging: false });
+      })
+      .catch(() => this.setState({ error: true }));
   }
   render() {
-    const { name, population, rotationPeriod, diameter, id } = this.state;
+    const { planet, planetLoaging, error } = this.state;
+
     return (
       <div className="random-planet jumbotron rounded">
-        {id && (
-          <img
-            className="planet-image"
-            alt="d"
-            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          />
+        {error ? (
+          <ErrorMessage />
+        ) : planetLoaging ? (
+          <Loading />
+        ) : (
+          <PlanetView planet={planet} />
         )}
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
       </div>
     );
   }
