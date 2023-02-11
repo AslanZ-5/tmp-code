1955bf919ed528602a9f1db741fc34c1d2f03b3b
Aslan <asl.zurabov@gmail.com>
2023-02-11 14:55:50 +0300

    react ts redux toolkit'

--- ts-cart/src/components/Cart.tsx
+++ ts-cart/src/components/Cart.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import useCart from "../hooks/useCart";
+import { useState } from "react";
+import CartLineItem from "./CartLineItem";
 
 const Cart = () => {
-  return (
-    <div>
-      <h1>CArt</h1>
-    </div>
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const { dispacht, REDUCER_ACTIONS, totalPrice, totalItems, cart } = useCart();
+
+  const onSubmitOrder = () => {
+    dispacht({ type: REDUCER_ACTIONS.SUBMIT });
+    setConfirm(true);
+  };
+  const pageContent = confirm ? (
+    <h2>Thank you for your order.</h2>
+  ) : (
+    <>
+      <h2 className="offscreen">Cart</h2>
+      <ul className="cart">
+        {cart.map((item) => {
+          return (
+            <CartLineItem
+              key={item.sku}
+              item={item}
+              dispacht={dispacht}
+              REDUCER_ACTIONS={REDUCER_ACTIONS}
+            />
+          );
+        })}
+      </ul>
+      <div className="cart__totals">
+        <p>Total Items: {totalItems}</p>
+        <p>Total Price: {totalPrice}</p>
+        <button
+          className="cart__submit"
+          disabled={!totalItems}
+          onClick={onSubmitOrder}
+        >
+          Place Order
+        </button>
+      </div>
+    </>
   );
+  const content = <main className="main main--cart">{pageContent}</main>;
+  return content;
 };
 
 export default Cart;
